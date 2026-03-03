2f5005fc7b918718f90e398264d14e73f50f6e02
amogusmp85-cloud <amogusmp85@gmail.com>
2026-03-03 14:51:54 +0800

    Initialize Discord bot with Express server
    Set up a Discord bot with Express server and basic commands.

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => res.send('Bot is alive'));
+app.listen(process.env.PORT || 3000);
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+  if (message.content === '!ping') message.reply('Pong!');
+});
+
+process.on('unhandledRejection', error => console.error(error));
+
+client.login(process.env.TOKEN);
